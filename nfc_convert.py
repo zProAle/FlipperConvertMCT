c7438c670fb0976d783ab7feb95d35274a0358f3
Alessandro Rossi <zproale@gmail.com>
2023-03-06 12:24:50 +0100

    fix a bug :)

--- nfc_convert.py
+++ nfc_convert.py
@@ -5,14 +5,13 @@ import time
 
 #Controllo che tipo di tag è e la dimensine del dump tramite il SAK
 def guess_mifare_size_by_sak(SAK):
-  if SAK == "18":
-    return "4K"
-  elif SAK == "08":
-    return "1K"
-  elif SAK == "88":
-    return "1K"
-  else:
-    return "idk"
+    sakk = {
+        "18": "4K",
+        "08": "1K",
+        "88": "1K",
+        "11": "4K",
+    }
+    return sakk.get(SAK, "1K")
 
 # aggiungo gli spazi ad ogni byte A0A1A2 -> A0 A1 A2
 def add_spaces_to_hex(in_str):
@@ -20,6 +19,14 @@ def add_spaces_to_hex(in_str):
   for i in range(0, len(in_str), 2):
     out_str += in_str[i:i+2] + " "
   return out_str.strip()
+
+#Tramite il primo byte del blocco 0 riesco a determinare in modo accurato se un uid è a 4/7 byte
+def check_uid_size(dump):
+    block0 = dump[0]
+    if (block0[0] != '0' and block0[1] != '8' and block0[1] != 'F'):
+        return 4
+    else: 
+        return 7
 
 #apro il file in formato nfc e creo un array con tutti i blocchi
 def convert_file(input_path):
@@ -43,14 +50,22 @@ def write_flipper_nfc(output_path, dump):
         f.write('# Nfc device type can be UID, Mifare Ultralight, Mifare Classic, Bank card\n')
         f.write('Device type: Mifare Classic\n')
         f.write('# UID, ATQA and SAK are common for all formats\n')
-        f.write('UID: '  + add_spaces_to_hex(dump[0][0:8])  + '\n')
-        f.write('ATQA: ' + add_spaces_to_hex(dump[0][12:16]) + '\n')
-        f.write('SAK: '  + add_spaces_to_hex(dump[0][10:12])  + '\n')
-        f.write('# Mifare Classic specific data\n')
-        if guess_mifare_size_by_sak(dump[0][10:12]) == "4K":
-            f.write('Mifare Classic type: 4K\n')
-        elif guess_mifare_size_by_sak(dump[0][10:12]) == "1K":
-            f.write('Mifare Classic type: 1K\n')
+        if(check_uid_size(dump) == 4):
+            f.write('UID: '  + add_spaces_to_hex(dump[0][0:8])  + '\n')
+            f.write('ATQA: ' + add_spaces_to_hex(dump[0][12:16]) + '\n')
+            f.write('SAK: '  + add_spaces_to_hex(dump[0][10:12])  + '\n')
+            if guess_mifare_size_by_sak(dump[0][10:12]) == "4K":
+                f.write('Mifare Classic type: 4K\n')
+            elif guess_mifare_size_by_sak(dump[0][10:12]) == "1K":
+                f.write('Mifare Classic type: 1K\n')
+        elif(check_uid_size(dump) == 7):
+            f.write('UID: '  + add_spaces_to_hex(dump[0][0:14])  + '\n')
+            f.write('ATQA: ' + add_spaces_to_hex(dump[0][16:20]) + '\n')
+            f.write('SAK: '  + add_spaces_to_hex(dump[0][14:16])  + '\n')
+            if guess_mifare_size_by_sak(dump[0][14:16]) == "4K":
+                f.write('Mifare Classic type: 4K\n')
+            elif guess_mifare_size_by_sak(dump[0][14:16]) == "1K":
+                f.write('Mifare Classic type: 1K\n')
         f.write('Data format version: 2\n')
         f.write('# Mifare Classic blocks, \'??\' means unknown data\n')
         for block in range(0, len(dump)):
@@ -68,7 +83,7 @@ def get_args():
     parser.add_argument(
         "-o",
         "--output-path",
-        required=False,
+        required=True,
         type=pathlib.Path,
         help="File di output, Es. nfc_convert.py -i file.dump -o file.nfc",
     )
